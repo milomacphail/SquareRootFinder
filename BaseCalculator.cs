a1fdd8a544c6ea2b36a90c455e559f9f9e833527
milomacphail <milomacphail@gmail.com>
2019-09-28 17:51:09 -0400

    Created Basic and Scientific Calculator classes.

---
+++ BaseCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class BaseCalculator
+    {
+        public void Multiply()
+        {
+            Console.WriteLine("Enter a number to multiply: ");
+
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another number to multiply: ");
+
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} * {1} = " + (first * second), first, second);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,16 @@
 ﻿using System;
 
-namespace SquareRootFinder
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double input = 0.0;
+            BaseCalculator testMultiply = new BaseCalculator();
+            testMultiply.Multiply();
 
-            double decimalPrecision = 0.0;
-
-            Console.WriteLine("Enter number to square root: ");
-            input = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter decimal place to which to round square root (use decimal): ");
-            decimalPrecision = Convert.ToDouble(Console.ReadLine());
-
-            if(input < 0)
-            {
-                Console.WriteLine("Input must be larger than 0");
-                return;
-            }
-
-            if (input == 0)
-            {
-                Console.WriteLine("The square root of 0 is 0. Please stop wasting my time and yours.");
-                return;
-            }
-
-            double inputByTen = input / 10;
-            double inputByInputByTen = input / inputByTen;
-
-            do
-            {
-                Console.WriteLine("Square root of " + input + ") = " + inputByTen);
-                inputByTen = (inputByTen + inputByInputByTen) / 2;
-                inputByInputByTen = input / inputByTen;
-            } while (inputByTen - inputByInputByTen > decimalPrecision);
-
-            Console.ReadKey();
+            ScientificCalculator testSquareRoot = new ScientificCalculator();
+            testSquareRoot.SquareRoot();
         }
     }
 }
